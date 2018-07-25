b4a4d3b13daec2403a5a1dde28ebfdd72999aa6a
Roberto García <roberto@rhizomik.net>
2018-07-25 16:02:18 +0200

    Manifest for single authorship using IPFS hash as key

--- contracts/CopyrightlyRegistry.sol
+++ contracts/CopyrightlyRegistry.sol
@@ -4,47 +4,35 @@ pragma solidity ^0.4.24;
 /// @author Roberto García (roberto@rhizomik.net)
 contract CopyrightlyRegistry {
 
-    struct Multihash {
-        bytes32 hash;
-        uint8 hashFunction;
-        uint8 size;
-    }
-
     struct Manifestation {
-        Multihash content;
         string title;
         address[] authors;
     }
 
-    event ManifestEvent(Manifestation manifestation);
+    event ManifestEvent(string hash, string title, address[] authors,
+        address indexed manifester);
 
-    mapping (bytes32 => Manifestation) private manifestations;
+    mapping (string => Manifestation) private manifestations;
 
     /// @notice Register single authorship for `msg.sender` of the manifestation with title `title`
-    /// and content hash `hash`, hash function `hashFunction` and hash size `size`.
+    /// and hash `hash`.
     /// @dev To be used when their is just one author, which is considered to be the message sender
-    /// @param hash Hash part of the content multihash
-    /// @param hashFunction Hash function of the content multihash
-    /// @param size Sike of the content multihash
+    /// @param hash Hash of the manifestation content, for instance IPFS Base58 Hash
     /// @param title The title of the manifestation
-    function manifestAuthorship(bytes32 hash, uint8 hashFunction, uint8 size, string title) public {
-        bytes32 index = keccak256(hash, hashFunction, size);
-        Multihash memory content = Multihash(hash, hashFunction, size);
-        Manifestation memory manifestation = Manifestation(content, title);
-        manifestation.authors.push(msg.sender);
-        manifestations[index] = manifestation;
-        emit ManifestEvent(manifestation);
+    function manifestAuthorship(string hash, string title) public {
+        require(manifestations[hash].authors.length == 0, "Manifestation already registered");
+        address[] memory authors = new address[](1);
+        authors[0] = msg.sender;
+        Manifestation memory manifestation = Manifestation(title, authors);
+        manifestations[hash] = manifestation;
+        emit ManifestEvent(hash, title, authors, msg.sender);
     }
 
-    /// @notice Retrieve the title and stake of manifestation with content
-    /// hash `hash`, hash function `hashFunction` and hash size `size`.
-    /// @param hash Hash part of the content multihash
-    /// @param hashFunction Hash function of the content multihash
-    /// @param size Sike of the content multihash
-    /// @return The title and stake of the manifestation
-    function getManifestation(bytes32 hash, uint8 hashFunction, uint8 size) public constant
+    /// @notice Retrieve the title and authors of the manifestation with content hash `hash`.
+    /// @param hash Hash of the manifestation content, for instance IPFS Base58 Hash
+    /// @return The title and authors of the manifestation
+    function getManifestation(string hash) public constant
     returns (string title, address[] authors) {
-        bytes32 index = keccak256(hash, hashFunction, size);
-        return (manifestations[index].title, manifestations[index].authors);
+        return (manifestations[hash].title, manifestations[hash].authors);
     }
 }
