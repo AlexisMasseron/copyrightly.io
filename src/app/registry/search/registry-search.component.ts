9419c6fa90d819f700ad71197e8fdd87ea3f5e15
Roberto García <roberto@rhizomik.net>
2018-08-03 07:39:19 +0200

    Use ngUnsubscribe Subject to unsubscribe Observable subscribes

--- src/app/registry/search/registry-search.component.ts
+++ src/app/registry/search/registry-search.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/internal/Subject';
 import { AlertsService } from '../../alerts/alerts.service';
 import { Web3Service } from '../../util/web3.service';
 import { RegistryContractService } from '../registry-contract.service';
 import { Manifestation } from '../manifestation';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-registry-search',
   templateUrl: './registry-search.component.html',
   styleUrls: ['./registry-search.component.css']
 })
-export class RegistrySearchComponent implements OnInit {
+export class RegistrySearchComponent implements OnInit, OnDestroy {
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
+
   manifestation = new Manifestation();
 
   constructor(private web3Service: Web3Service,
@@ -19,7 +23,7 @@ export class RegistrySearchComponent implements OnInit {
   ngOnInit(): void { }
 
   getManifestation(hash: string) {
-    this.registryContractService.getManifestation(hash)
+    this.registryContractService.getManifestation(hash).pipe(takeUntil(this.ngUnsubscribe))
       .subscribe((manifestation: Manifestation) => {
         this.manifestation = manifestation;
         if (!this.manifestation.title) {
@@ -28,5 +32,10 @@ export class RegistrySearchComponent implements OnInit {
       }, error => {
         this.alertsService.error(error);
       });
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
