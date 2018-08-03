cd4e2c3f31b290083888a4bd0267df39a4990b7b
Roberto García <roberto@rhizomik.net>
2018-08-03 05:58:25 +0200

    Add Event and ManifestationEvent classes

---
+++ src/app/registry/manifestation-event.ts
@@ -0,0 +1,21 @@
+import { Event } from './event';
+import { Manifestation } from './manifestation';
+
+export class ManifestationEvent extends Event {
+  what: Manifestation;
+
+  constructor(values: Object = {}) {
+    super(values);
+    this.what = values['what'];
+  }
+
+  toHTML(): string {
+    return '<h5>' + this.type.substring(0, this.type.indexOf('Event')) + '</h5>' +
+           '<p> Who: ' + this.who + '</p>' +
+           '<p> When: ' + this.when + '</p>' +
+           '<h6> What: </h6>' +
+           '<p>Title: ' + this.what.title + '</p>' +
+           '<p>Authors: ' + this.what.title + '</p>' +
+           '<p>Hash: ' + this.what.hash + '</p>';
+  }
+}
--- src/app/registry/manifestation.ts
+++ src/app/registry/manifestation.ts
@@ -1,5 +1,4 @@
 export class Manifestation {
-
   hash: string;
   title: string;
   authors: string[];
