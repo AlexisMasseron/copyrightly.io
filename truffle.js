b2f3816688588c3e07297c80aa6ed9fabebbef6c
Roberto García <roberto@rhizomik.net>
2018-08-16 02:14:27 +0200

    Enable deployment to Ropsten

--- truffle.js
+++ truffle.js
@@ -12,7 +12,10 @@
  *   },
  */
 
-const fullPathBuildDirectory = `${__dirname}/src/assets`;
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+
+const fullPathBuildDirectory = `${__dirname}/src/assets/contracts`;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -24,6 +27,15 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC,
+          "https://ropsten.infura.io/" + process.env.INFURA_TOKEN, 0, 2)
+      },
+      network_id: 3,
+      gas: 1700000,
+      gasPrice: 1000000000
     }
   }
 };
