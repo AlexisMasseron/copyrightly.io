420780cdeedf44d71a5bbbd092243e917292812b
Roberto García <roberto@rhizomik.net>
2018-08-22 14:21:01 +0200

    Enable onlyHash option without file upload to IPFS

--- src/app/util/ipfs.service.ts
+++ src/app/util/ipfs.service.ts
@@ -12,13 +12,13 @@ export class IpfsService {
     this.ipfsApi = IPFS_API('ipfs.infura.io', '5001', {protocol: 'https'});
   }
 
-  public uploadFile (file): Observable<string> {
+  public uploadFile (file, uploadToIpfs): Observable<string> {
     return new Observable((observer) => {
       this.ngZone.runOutsideAngular(() => {
         const reader = new FileReader();
         reader.onprogress = (progress) => console.log(`Loaded: ${progress.loaded}/${progress.total}`);
         reader.onloadend = () => {
-          this.saveToIpfs(reader).subscribe((hash: string) => {
+          this.saveToIpfs(reader, uploadToIpfs).subscribe((hash: string) => {
             this.ngZone.run(() => {
               observer.next(hash);
               observer.complete();
@@ -31,11 +31,11 @@ export class IpfsService {
     });
   }
 
-  private saveToIpfs(reader): Observable<string> {
+  private saveToIpfs(reader, uploadToIpfs): Observable<string> {
     return new Observable((observer) => {
       this.ngZone.runOutsideAngular(() => {
         const buffer = Buffer.from(reader.result);
-        this.ipfsApi.add(buffer, {progress: (progress) => console.log(`Saved: ${progress}`)})
+        this.ipfsApi.add(buffer, {onlyHash: !uploadToIpfs, progress: (progress) => console.log(`Saved: ${progress}`)})
         .then((response) => {
           console.log(`IPFS_ID: ${response[0].hash}`);
           observer.next(response[0].hash);
